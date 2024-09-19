e8587f3c35a6e9eb1ce005840d67b35bb0dd3184
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-19 15:03:28 -0300

    Concorrência com select

--- 18 - Concorrência/5 - select/select.go
+++ 18 - Concorrência/5 - select/select.go
@@ -10,7 +10,14 @@ Nesse cenário a primeira fun tem seu funcionamento prejudicado pela segunda fun
 
 O canal 1 é lido primeiro e precisa de menos tempo, e o canal 2 é lido depois e precisa de mais tempo, então a primeira função fica limitada
 ao tempo de execução da segunda.
+
+Para resolver isso, é necessário usar o select, que permite que um canal
+seja lido em paralelo a outro.
+
+O select é uma estrutura de controle que permite aguardar múltiplos canais
+em paralelo e executar a ação correspondente quando algum deles estiver pronto.
 */
+
 func main() {
 	canal1, canal2 := make(chan string), make(chan string)
 
@@ -29,10 +36,11 @@ func main() {
 	}()
 
 	for {
-		mensagemCanal1 := <-canal1
-		fmt.Println(mensagemCanal1)
-
-		mensagemCanal2 := <-canal2
-		fmt.Println(mensagemCanal2)
+		select {
+		case mensagemCanal1 := <-canal1:
+			fmt.Println(mensagemCanal1)
+		case mensagemCanal2 := <-canal2:
+			fmt.Println(mensagemCanal2)
+		}
 	}
 }
