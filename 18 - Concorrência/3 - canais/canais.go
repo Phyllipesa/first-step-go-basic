e86f89591f0f3d8ad63ecb2e08a21bb756f4778f
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-19 14:43:55 -0300

    Canais com buffer

--- 18 - Concorrência/3 - canais/canais.go
+++ 18 - Concorrência/3 - canais/canais.go
@@ -5,6 +5,16 @@ import (
 	"time"
 )
 
+// Canal (Channel) - É a forma de comunicação entre goroutines.
+// Canal é um tipo de dado que permite a comunicação entre goroutines.
+// Para criar um canal, usamos a palavra-chave `make(chan tipo)`.
+// O canal é bidirecional, ou seja, pode enviar e receber valores.
+// O canal é bloqueante, ou seja, se o canal estiver cheio e você tentar enviar um valor, o programa vai bloquear até que algum valor seja recebido.
+// O canal é unidirecional, ou seja, pode enviar valores mas não pode receber.
+// O canal é fechado, ou seja, quando todos os valores foram enviados, o canal é fechado e não pode receber mais valores.
+// O canal é bufferizado, ou seja, pode armazenar valores em um buffer e enviar ou receber valores de um buffer.
+// O canal é sincronizado, ou seja, só uma goroutine pode enviar ou receber valores de um canal por vez.
+// O canal é de tipo genérico, ou seja, pode armazenar valores de qualquer tipo.
 func main() {
 	canal := make(chan string)
 	go escrever("Olá mundo", canal)
--- 18 - Concorrência/4 - canais com buffer/canais-com-buffer.go
+++ 18 - Concorrência/4 - canais com buffer/canais-com-buffer.go
@@ -4,10 +4,22 @@ import (
 	"fmt"
 )
 
+// Canal (Channel) - É a forma de comunicação entre goroutines.
+// Canal é um tipo de dado que permite a comunicação entre goroutines.
+// Para criar um canal, usamos a palavra-chave `make(chan tipo)`.
+// O canal é bufferizado, ou seja, pode armazenar valores em um buffer e enviar ou receber valores de um buffer.
+
+// Exemplo de canal bufferizado
 func main() {
-	canal := make(chan string)
-	canal <- "Olá mundo!" // deadlock!
+	canal := make(chan string, 2)
+	canal <- "Olá mundo!"
+	canal <- "Programando em Go!"
+
+	// Caso eu tente mandar ou receber outro valor acarretará em um deadlock, porque excediderá a quantidade do buffer
 
 	msg := <-canal
+	msg2 := <-canal
+
 	fmt.Println(msg)
+	fmt.Println(msg2)
 }
