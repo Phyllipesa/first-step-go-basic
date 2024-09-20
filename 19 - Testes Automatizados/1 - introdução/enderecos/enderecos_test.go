7cef2ec52a9025e8a706fe92d02ce77a8943542f
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-20 16:23:22 -0300

    alterando importação, nome do package, testes paralelos e comandos de testes

--- 19 - Testes Automatizados/1 - introdução/enderecos/enderecos_test.go
+++ 19 - Testes Automatizados/1 - introdução/enderecos/enderecos_test.go
@@ -1,16 +1,28 @@
 // TESTE DE UNIDADE
 
-package enderecos
+package enderecos_test
 
-import "testing"
+import (
+	. "introducao-testes/enderecos"
+	"testing"
+)
 
 type cenarioDeTeste struct {
 	enderecoInserido string
 	retornoEsperado  string
 }
 
+// go test ./... = esse comando faz com que o go execute todos os testes do projeto
+// go test -v = esse comando mostra mais informações durante o teste
+// go test --cover = exibe a cobertura dos seus testes
+// go test --coverprofile nome-do-arquivo.txt = Faz um arquivo contendo um relatório sobre as linhas que estão cobertas ou não pelos testes
+// go tool cover --func=cobertura.txt = Mostra todas as funções que estão sendo testadas de acordo com o relatório.
+// go tool cover --html=cobertura.txt = Mostra em um arquivo HTML todas as linhas que não estão cobertas por testes.
+
 // TipoDeEndereco verifica se um endereço tem um tipo válido e o retorna
 func TestTipoDeEndereco(t *testing.T) {
+	// t.Parallel() = faz com que o teste seja executado paralelamente com outros
+	// Fica na primeira linha e precisa ser colocado em todo o teste que executará paralelamente
 
 	cenariosDeTeste := []cenarioDeTeste{
 		{"Rua ABC", "Rua"},
